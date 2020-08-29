5d5160b2715fb3d698598f77fc68c8e3fe04a334
lordefps <lodizinho_mice@hotmail.com>
2020-08-29 14:57:35 -0300

    Mostrando -> %

---
+++ Aula 3/impar_par.py
@@ -0,0 +1,12 @@
+# fazer um codigo para dizer se o numero é par ou impar
+
+
+valor = int(input("Digite um numero: "))
+
+
+if (valor % 2) == 0:
+
+    print("Numero par")
+
+else: 
+    print("Numero impar")
