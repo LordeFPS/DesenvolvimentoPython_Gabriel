a866482d33e0290926701c2598c41b68255e7afd
lordefps <lodizinho_mice@hotmail.com>
2020-08-29 18:39:46 -0300

    Bug do DB

---
+++ Aula3/Projeto/Banco_de_Dados/conexao_BD.py
@@ -0,0 +1,6 @@
+# acesso do Banco de dados
+
+import sqlite3
+
+conexao = sqlite3.connect(r"C:\Users\69146\Desktop\Desenvolvimento Agil Python\DesenvolvimentoPython_Gabriel_2020\Aula3\sqlite3\pesquisa.db")
+
