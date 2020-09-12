decd4db395f33da1e247b465060f838fbb10cfbb
lordefps <lodizinho_mice@hotmail.com>
2020-09-12 15:29:43 -0300

    função_lista_ex.8

--- Revisao/exemplo8.py
+++ Revisao/exemplo8.py
@@ -1,19 +1,19 @@
-lista   = [ "Água", "Peixe", "Golfinho", "Tubarão"]
-# índice      0        1          2           3
+lista   = [ "Água", "Peixe", "Golfinho", "Tubarão","Sereia"]
+# índice      0        1          2           3       4
 
 # Recuperando elementos da lista
 
 # "Golfinho"
-
+print(lista[2])
 # "Peixe"
-
+print(lista[1])
 # "Água"
-
+print(lista[0])
 # "Tubarão"
-
+print(lista[3])
 
 # Pegar o ultimo elemento da lista independente da posição
-
+print(lista[-1]) # numero -1 que vai pegar sempre o ultimo elemento da lista
 
 
 
